77fb4903854eb251e690eb6469db9fc9657ec3a3
Yanqin Qualmann <yq.qualmann@gmail.com>
2023-12-25 22:09:12 +0100

    png

--- src/pages/Informationen.tsx
+++ src/pages/Informationen.tsx
@@ -102,7 +102,7 @@ const responsiveSettings = [
           <div className="each-slide">
             <div
               style={{
-                backgroundImage: `url(https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/schwein.jpg)`
+                backgroundImage: `url(https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/schwein.png)`
               }}
             >
               <span>
--- src/pages/Infos/Preise.tsx
+++ src/pages/Infos/Preise.tsx
@@ -1,13 +1,11 @@
 export default function Preise() {
     return (
-        <>
-        <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/schwein.jpg" alt="preise" />
+      <>
+        <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/schwein.png" alt="preise" />
         <div className="textContainer">
           <h1>Preise</h1>
         </div>
       </>
        
-            
-    
     )
 }
