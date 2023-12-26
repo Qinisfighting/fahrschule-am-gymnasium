8269d7d171de83f17abcb625f97caaa0f736a461
Yanqin Qualmann <yq.qualmann@gmail.com>
2023-12-26 16:00:41 +0100

    weiter

--- src/pages/Infos/Allgemeines.tsx
+++ src/pages/Infos/Allgemeines.tsx
@@ -1,10 +1,65 @@
 export default function Allgemeines() {
     return (
         <>
-        <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/fahren.jpg" alt="fahren" />
-        <div className="textContainer">
+          <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/fahren.jpg" alt="fahren" />
+          <div className="textContainer">
             <h1>Allgemeines</h1>
-        </div>
+            <p>
+              In Deutschland muss man vieles mit einem Formular beantragen. So auch den Erwerb einer Fahrerlaubnis. Dafür gibt es einen Antrag. Diesen Antrag füllen wir für dich aus, den du dann unterschreiben musst.
+            </p>
+            <p>
+              Für die erste Erteilung einer Fahrerlaubnis benötigen wir:
+            </p>
+            <ul>
+                <li>Passbild, biometrisch ( 35 X 45 mm )</li>
+                <li>Sehtest (Optiker)</li>
+                <li>Bescheinigung über Erste Hilfe ( z.B. DRK, ASB )</li>
+            </ul>
+            <p>
+              Solltest Du bereits einen Führerschein besitzen:
+            </p>
+            <ul>
+                <li>Neues Passbild, biometrisch ( 35 X 45 mm )</li>
+                <li>Sehtest</li>
+                <li>Erste Hilfe Bescheinigung</li>
+            </ul>
+            <p>
+              Den Antrag geben wir bei dem Einwohnermeldeamt ab, wo du gemeldet bist.
+            </p>
+            <p>
+              Das kostet 5,10 €
+            </p>
+            <p>
+              Das Einwohnermeldeamt leitet den Antrag weiter an die Führerscheinstelle in Eutin. Die schicken dir dann eine Rechnung, bitte evt. Information genau lesen, die du bitte überweist. Ist das Geld dort eingegangen, stellt die Führerscheinstelle einen Prüfauftrag an den Tüv in Lübeck. Der Tüv schickt dir dann auch eine Rechnung, die wiederum bezahlt werden muss, sonst prüft der Tüv nicht. Ist die Zahlung eingegangen, bekommen wir eine Mitteilung.
+            </p>
+            <h2>Grundunterricht</h2>
+            <p>
+              Bei Ersterwerb einer Fahrerlaubnis musst du in der Fahrschule an einem Grundunterricht teilnehmen.
+            </p>
+            <p>
+            Für die entsprechende Klasse kommt dann noch Zusatzstoff hinzu:
+            </p>
+            <p>
+            Klasse B ---- 2 x 90 min <br />
+            Klasse A1, A2, A ----  4 x 90 min <br />
+            Klasse M, AM ----  2 x 90 min <br />
+            Klasse T ----  6 x 90 min <br />
+            </p>
+            <p>
+            Beim Ersterwerb Klasse B also insgesamt 14 x 90 min, wobei die beiden Themen des Zusatzunterrichts gemacht werden müssen, hier Thema 13 und 14.
+            </p>
+            <p>
+            Bei einer Erweiterung sind 6 x 90 min Grundunterricht, sowie der Zusatzstoff vorgeschrieben.
+            </p>
+            <p>
+            Die praktische Ausbildung, das Fahren also, soll zum Unterricht parallel stattfinden.
+            </p>
+            <h2>Wie oft musst du fahren?</h2>
+            <p>
+            Wie viele Übungsfahrten man braucht hängt von jedem individuell ab.
+            </p>
+
+          </div>
         </>
     )
 }
