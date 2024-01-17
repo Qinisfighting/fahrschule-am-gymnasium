e4345c9af30cf2fd36f4d9d0306bf2790539d2df
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-17 22:40:58 +0100

    Update Theorie.tsx

--- src/pages/Infos/Theorie.tsx
+++ src/pages/Infos/Theorie.tsx
@@ -39,11 +39,11 @@ export default function Theorie() {
           <div className="t-kalendar"> 
             <h2>Kalender</h2>  
             <h3>Di. & Do. 19:00 - 20:30 Oldenburg i.H.</h3>  
-            <iframe src="https://calendar.google.com/calendar/embed?src=fahrschuleamgymnasium%40gmail.com&ctz=Europe%2FBerlin" width="100%" height="300"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?src=fahrschuleamgymnasium%40gmail.com&ctz=Europe%2FBerlin" width="100%" height="400"></iframe>
             <br />
             <br />
             <h3>Mo. & Mi. 19:00 - 20:30 Lensahn</h3>
-            <iframe src="https://calendar.google.com/calendar/embed?src=d85d0c350088a031fa0d7d7f262f0d7461727394d326f98e697036e4cd2fcdec%40group.calendar.google.com&ctz=Europe%2FBerlin" width="100%" height="300"></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?src=d85d0c350088a031fa0d7d7f262f0d7461727394d326f98e697036e4cd2fcdec%40group.calendar.google.com&ctz=Europe%2FBerlin" width="100%" height="400"></iframe>
           </div>
           {/* <div className="theorieprüfung">   
             <h1>Theorieprüfung</h1>
