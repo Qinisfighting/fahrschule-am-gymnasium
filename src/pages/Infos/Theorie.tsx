9c016fd16cf2f0b1051fe864fffd95d76b198ea7
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-01-17 14:33:07 +0100

    theorie

--- src/pages/Infos/Theorie.tsx
+++ src/pages/Infos/Theorie.tsx
@@ -1,35 +1,45 @@
 export default function Theorie() {
     return (
       <>
-        <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/theorie.jpg" alt="Theorieprüfung" />
+        <img className="info-img" src="https://raw.githubusercontent.com/Qinisfighting/fahrschule-am-gymnasium/main/src/assets/theorie.jpg" alt="Theorieprüfung" />  
+        <h1 className="theorie--title">Theorieunterricht</h1>
         <div className="textContainer">
           
-          <div className="theorieunterricht">    
+          {/* <div className="theorieunterricht">    
               <div className="theorieunterricht--left">
-                  <h1>Theorieunterricht</h1>
+                  <h1>Themen</h1>
               </div>
               <div className="theorieunterricht--right">
                   <p>Oldenburg: Di. & Do. 19:00 - 20:30</p>
                   <p>Lensahn: Mo. & Mi. 19:00 - 20:30</p>
               </div>      
+          </div> */}
+          <h2>Themen</h2>
+          <div className="theorieunterricht--thema">
+            <div className="theorieunterricht--thema-left">
+              <p>Thema 1: Persönliche Voraussetzungen</p>
+              <p>Thema 2: Risikofaktor Mensch</p>
+              <p>Thema 3: Rechtliche Rahmenbedingungen</p>
+              <p>Thema 4: Straßenverkehrssystem und Bahnübergänge</p>
+              <p>Thema 5: Grundregel, Vorfahrt und Verkehrszeichen</p>
+              <p>Thema 6. Verkehrszeichen und Verkehrseinrichtungen</p>
+              <p>Thema 7: Teilnehmer am Straßenverkehr – Besonderheiten und Verhalten</p>     
+            </div>
+            <div className="theorieunterricht--thema-right">
+              <p>Thema 8: Geschwindigkeit, Abstand und Umweltschonende Fahrweise</p>
+              <p>Thema 9: Verkehrsbeobachtung und Verkehrsverhalten bei Fahrmanövern</p>
+              <p>Thema 10: Ruhender Verkehr</p>
+              <p>Thema 11: Verhalten in besonderen Situationen, Folgen von Verstößen gegen Verkehrsvorschriften</p>
+              <p>Thema 12: Lebenslanges Lernen</p>
+              <p>Thema 13 (B1): Technische Bedingungen, Personen- und Güterbeförderung Umweltschutz</p>
+              <p>Thema 14 (B2): Fahren mit Solokraftfahrzeugen und Zügen</p>
+            </div>  
           </div>
-          <div className="theorieunterricht--bottom">
-            <h3>Thema 1: Persönliche Voraussetzungen</h3>
-            <h3>Thema 2: Risikofaktor Mensch</h3>
-            <h3>Thema 3: Rechtliche Rahmenbedingungen</h3>
-            <h3>Thema 4: Straßenverkehrssystem und Bahnübergänge</h3>
-            <h3>Thema 5: Grundregel, Vorfahrt und Verkehrszeichen</h3>
-            <h3>Thema 6. Verkehrszeichen und Verkehrseinrichtungen</h3>
-            <h3>Thema 7: Teilnehmer am Straßenverkehr – Besonderheiten und Verhalten</h3>
-            <h3>Thema 8: Geschwindigkeit, Abstand und Umweltschonende Fahrweise</h3>
-            <h3>Thema 9: Verkehrsbeobachtung und Verkehrsverhalten bei Fahrmanövern</h3>
-            <h3>Thema 10: Ruhender Verkehr</h3>
-            <h3>Thema 11: Verhalten in besonderen Situationen, Folgen von Verstößen gegen Verkehrsvorschriften</h3>
-            <h3>Thema 12: Lebenslanges Lernen</h3>
-            <h3>Thema 13 (B1): Technische Bedingungen, Personen- und Güterbeförderung Umweltschutz</h3>
-            <h3>Thema 14 (B2): Fahren mit Solokraftfahrzeugen und Zügen</h3>
+          <div className="t-kalendar">
+            <h2>Kalender</h2>
+            <iframe src="https://calendar.google.com/calendar/embed?src=4q6gj7jv7q8l7r3o0q4jvq0l5g%40group.calendar.google.com&ctz=Europe%2FBerlin" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
           </div>
-          <div className="theorieprüfung">   
+          {/* <div className="theorieprüfung">   
             <h1>Theorieprüfung</h1>
             <ul>
               <li>Die theoretische Führerscheinprüfung wird von Prüfstellen wie TÜV oder Dekra abgenommen.</li>
@@ -45,7 +55,7 @@ export default function Theorie() {
                 <br />
               <li>Die Theorieprüfung kostet Sie ungefähr 25 Euro.</li>        
             </ul>
-          </div>   
+          </div>    */}
         </div>
         </>  
     )
